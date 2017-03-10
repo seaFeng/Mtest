91082c5bd02f05aebf6a39e49a08fb0dddbbee76
dahai <1572179344@qq.com>
2017-03-10 17:26:59 +0800

    metrial Design 的主Activity的生成

---
+++ app/src/main/java/com/haige/mtest/MaterialDesign/MetrialActivity.java
@@ -0,0 +1,15 @@
+package com.haige.mtest.MaterialDesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.haige.mtest.R;
+
+public class MetrialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_metrial);
+    }
+}
